3c4aad2012f80deacddddcf8c481d3dd12d18661
Shoji Miyanishi <omiya@smk.co.jp>
2017-12-19 13:10:32 +0900

    Initial Commit

---
+++ mmSensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mmSensor
+//
+//  Created by 宮西 昭次 on H29/12/19.
+//  Copyright © 平成29年 宮西 昭次. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
